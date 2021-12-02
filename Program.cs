b83c35409f8296d5870f05c1b65e2d5b4a12aeaf
JenniferSecib <jennifer.gullung.ext@secib.fr>
2021-12-02 14:05:39 +0100

    split string instead of regex

--- Program.cs
+++ Program.cs
@@ -10,16 +10,14 @@ namespace DiveFirstPart
             string[] commands = System.IO.File.ReadAllLines(@"C:\Users\j.gullung\source\repos\DiveFirstPart\input.txt");
             int horizontalPosition = 0;
             int depth = 0;
-            Regex filter = new(@"\w+"); // Equivalent to [a-zA-Z0-9_]
             string command;
             int units;
 
             for (int i = 0; i < commands.Length; i++)
             {
-                var matches = filter.Matches(commands[i]);
-
-                command = matches[0].Value;
-                units = Int32.Parse(matches[1].Value);
+                string[] commandAndUnit = commands[i].Split(' ');
+                command = commandAndUnit[0];
+                units = Int32.Parse(commandAndUnit[1]);
 
                 switch (command)
                 {
